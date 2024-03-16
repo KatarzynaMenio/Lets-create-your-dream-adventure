d6f3e8538e6bd68b1e6798d3f502a5515725eee2
KatarzynaMenio <meniokatarzyna@gmail.com>
2024-03-16 23:25:40 +0100

    6 - Create footer

--- src/view/Layout.jsx
+++ src/view/Layout.jsx
@@ -3,9 +3,15 @@ import Footer from "../components/Footer.jsx";
 import {Outlet} from "react-router-dom";
 
 export default function Layout() {
-    return (<>
-        <Navigation/>
-        <Outlet/>
-        <Footer/>
-    </>)
+    return (
+        <div className="layout">
+            <div className="content">
+                <Navigation/>
+                <Outlet/>
+            </div>
+            <div className="footer-wrapper">
+                <Footer/>
+            </div>
+        </div>
+    )
 }
