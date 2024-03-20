a1a8ae092690a215ab14d3eda05944aafd2224ef
KatarzynaMenio <meniokatarzyna@gmail.com>
2024-03-20 22:42:02 +0100

    9 - Create map component

---
+++ src/components/MapUSA.jsx
@@ -0,0 +1,8 @@
+import usa_map from '../assets/MapUSA/map-usa.svg'
+
+export default function MapUSA() {
+    return (
+        <div className="map-style">
+            <img src={usa_map} alt="usa_map" className="img-style"/>
+        </div>)
+}
