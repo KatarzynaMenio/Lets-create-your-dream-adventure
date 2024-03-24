fd066e1e15e2a1979fab45ef57434eb5519e9d70
KatarzynaMenio <meniokatarzyna@gmail.com>
2024-03-24 15:09:15 +0100

    5 - Fix navigation - connection to form

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -2,8 +2,22 @@ import LandingPage from "../components/LandingPage.jsx";
 import MapUSA from "../components/MapUSA.jsx";
 import Form from "../components/FormSection.jsx";
 import WhatYouGet from "../components/WhatYouGet.jsx";
+import {useEffect} from "react";
+import {useLocation} from "react-router-dom";
 
 export default function Home() {
+
+    const { state } = useLocation();
+    const { targetId } = state || {};
+
+    useEffect(() => {
+        const el = document.getElementById(targetId);
+        if (el) {
+            el.scrollIntoView();
+        }
+        console.log(targetId)
+    }, [targetId]);
+
     return (<div className="container">
         <LandingPage/>
         <MapUSA/>
